617ef39f22849464588f9b99971f278fb1059c5b
dmt041104111003 <daomanhtung4102003@gmail.com>
2025-10-04 19:19:07 +0700

    feat: background motion

--- src/components/project/ProjectPageClient.tsx
+++ src/components/project/ProjectPageClient.tsx
@@ -2,7 +2,6 @@
 import { useState, useEffect, Suspense } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { useSearchParams, useRouter, usePathname } from "next/navigation";
-import { motion } from "framer-motion";
 import ProjectCard from "~/components/project-card";
 import ProjectModal from "~/components/project-modal";
 import ProjectSkeleton from "~/components/project/ProjectSkeleton";
@@ -167,23 +166,6 @@ function ProjectPageContent() {
   
   return (
     <main className="relative pt-20 bg-white dark:bg-gradient-to-br dark:from-gray-950 dark:via-gray-950 dark:to-gray-900">
-      {/* Background Logo */}
-      <motion.div 
-        initial={{ opacity: 0, scale: 0.8 }}
-        whileInView={{ opacity: 0.15, scale: 1 }}
-        viewport={{ once: false, amount: 0.3 }}
-        transition={{ duration: 1, ease: "easeOut" }}
-        className="fixed left-[-200px] top-1/2 -translate-y-1/2 z-0 pointer-events-none select-none block"
-      >
-        <img
-          src="/images/common/loading.png"
-          alt="Cardano2VN Logo"
-          className="w-[1200px] h-[1200px] object-contain"
-          draggable={false}
-          style={{ objectPosition: 'left center' }}
-        />
-      </motion.div>
-      
       <div className="mx-auto max-w-7xl px-6 py-20 lg:px-8">
         <div>
           <Title
